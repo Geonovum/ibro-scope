0ab7b3f2ab92230c6d4f0286d52d2c762b8f269a
lvdbrink <l.vandenbrink@geonovum.nl>
2025-02-18 13:55:49 +0100

    update config, ch02-03-04

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "Integrale bronhouderregistratie (IBRO)",
+  title: "Integrale Bronregistratie Objecten (IBRO)",
   subtitle: "Scopedocument",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
